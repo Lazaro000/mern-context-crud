8ba665ba24902170305f5f75e3acb300c46bb3a2
Lázaro <lazaroyaesta@gmail.com>
2022-07-06 18:49:14 +0200

    ✨ React App Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { PostForm, HomePage, NotFoundPage } from './pages';
+import {Routes, Route} from 'react-router-dom';
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+      <div className='px-10 container m-auto'>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/new' element={<PostForm/>}/>
+          <Route path='*' element={<NotFoundPage/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
+export default App;
